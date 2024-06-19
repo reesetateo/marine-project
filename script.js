646f63e180742fff2f74ab9a13e2c15b7155975e
reesetateo <reese.l.tateo@vanderbilt.edu>
2024-06-18 22:22:34 -0400

    idek

--- script.js
+++ script.js
@@ -33,9 +33,11 @@ document.addEventListener('DOMContentLoaded', () => {
     function touchMove(event) {
         if (draggedItem) {
             let touch = event.touches[0];
-            draggedItem.style.position = 'absolute';
-            draggedItem.style.left = touch.clientX - draggedItem.clientWidth / 2 + 'px';
-            draggedItem.style.top = touch.clientY - draggedItem.clientHeight / 2 + 'px';
+            // draggedItem.style.position = 'absolute';
+            // draggedItem.style.left = touch.clientX - draggedItem.clientWidth / 2 + 'px';
+            // draggedItem.style.top = touch.clientY - draggedItem.clientHeight / 2 + 'px';
+            draggedItem.style.transform = `translate(${touch.clientX}px, ${touch.clientY}px)`;
+
             
         }
     }
